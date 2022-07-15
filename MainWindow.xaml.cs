e73f15dd54e689f3dafc970d347ace39b8e7e33f
raymond wang <me@waymondrang.com>
2022-07-15 15:43:46 -0700

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Something
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            ExtendsContentIntoTitleBar = true;  // enable custom titlebar
+            SetTitleBar(AppTitleBar);
+        }
+
+        private void HomePage_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer", "http://waymondrang.com");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer", "http://mc.waymondrang.com");
+        }
+    }
+}
